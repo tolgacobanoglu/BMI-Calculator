35ad8a2ed5b384f8a36a77188847524d87d543bc
tolgacobanoglu <1tolgacobanoglu@gmail.com>
2022-08-30 11:05:53 +0300

    last commit

--- app/src/main/java/com/tolgacobanoglu/bmicalculatorproject/Review.java
+++ app/src/main/java/com/tolgacobanoglu/bmicalculatorproject/Review.java
@@ -11,9 +11,11 @@ import com.google.android.play.core.tasks.Task;
 
 /**
  * It will work for activity
+ * This structure has a quota and if over quota it wont be work for quota https://developer.android.com/guide/playcore/in-app-review#quotas
  */
 public class Review {
 
+    //iki görevi var inceleme bilgilerini almak ve diğeri inceleme akışını başlatmak
     private final ReviewManager reviewManager;
 
     public Review(Context context) {
@@ -21,9 +23,12 @@ public class Review {
     }
 
     public void doRate(Context context) {
+        //request asenkron bir çağrı,
         Task <ReviewInfo> request = reviewManager.requestReviewFlow();
+        //asenkron olduğu için tamamlanmasını beklemeleyiz
         request.addOnCompleteListener(task -> {
             if (task.isSuccessful()){
+                //inceleme akışını başlatmak için kullanılan gerekli bilgileri tutan sınıf
                 ReviewInfo reviewInfo = task.getResult();
                 Task <Void> flow = reviewManager.launchReviewFlow((Activity) context, reviewInfo);
                 flow.addOnCompleteListener(task1 -> Toast.makeText(context, "Flow finished", Toast.LENGTH_SHORT).show());
